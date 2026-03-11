06750d91ff78030fddd88732c94dff370b24519e
jair-ap <jairalvarengapereira@gmail.com>
2026-03-11 10:28:51 -0300

    Up 11/03/2026 10:28

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -43,53 +43,67 @@ function Login(){
     }
   }
 
-  return<div className="row">
-    <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
-      <form className="frm-signin">
-        <img src={logo} className="logo mb-4" />
-        <h5 className="mb-5">Gerencie seus agendamentos de forma descomplicada.</h5>
-        <h5 className="mb-4 text-secondary">Acesse sua conta</h5>
+  return (
+    <div className="row">
+      <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
+        <form className="frm-signin">
+          <img src={logo} className="logo mb-4" />
+          <h5 className="mb-5">
+            Gerencie seus agendamentos de forma descomplicada.
+          </h5>
+          <h5 className="mb-4 text-secondary">Acesse sua conta</h5>
 
-        <div className="mt-4">
-          <input type="email" placeholder="E-mail" required 
-            className="form-control"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className="mt-2">
-          <input type="password" placeholder="Senha" required 
-            className="form-control"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className="mt-3 mb-5">
-          <button onClick={executeLogin} type="button" className="btn btn-primary w-100">
-            Login
-          </button>
-        </div>
-
-        { //Tratamento de erro - Condição de se msg existir, então (&&) exibe a mensagem abaixo
-          msg.length > 0 && 
-          <div className="alert alert-danger" role="alert">
-            {msg}
+          <div className="mt-4">
+            <input
+              type="email"
+              placeholder="E-mail"
+              required
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-        }
 
-        <div>
-          <span className="me-1">Não tenho uma conta. </span> 
+          <div className="mt-2">
+            <input
+              type="password"
+              placeholder="Senha"
+              required
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-3 mb-5">
+            <button
+              onClick={executeLogin}
+              type="button"
+              className="btn btn-primary w-100"
+            >
+              Login
+            </button>
+          </div>
+
+          {
+            //Tratamento de erro - Condição de se msg existir, então (&&) exibe a mensagem abaixo
+            msg.length > 0 && (
+              <div className="alert alert-danger" role="alert">
+                {msg}
+              </div>
+            )
+          }
+
+          <div>
+            <span className="me-1">Não tenho uma conta. </span>
             <Link to="/register"> Criar conta agora.</Link>
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
 
+      <div className="col-sm-7 d-none d-sm-block">
+        <img src={fundo} className="background-login" />
+      </div>
     </div>
-
-    <div className="col-sm-7">
-      <img src={fundo} className="background-login" />
-    </div>
-
-  </div>
+  );
 }
 
 export default Login;
